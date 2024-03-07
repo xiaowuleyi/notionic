9cb60d5f70beccfd0cad1ddad37c169eb89ce330
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-07 15:26:31 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,9 +2,9 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 350" width="600" height="350">
-  <rect width="600" height="350" fill="#cccccc"></rect>
-  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="monospace" font-size="26px" fill="#333333">XW</text>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+  <rect width="24" height="24" fill="#FFFFFFFF"></rect>
+  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="monospace" font-size="17px" fill="#333333">xw</text>
 </svg>
 )
 
