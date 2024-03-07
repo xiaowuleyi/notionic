b525b91032a59f89191c0a9a7e363d9fd005f163
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-07 21:10:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   transpilePackages: ['dayjs'],
   images: {
-    domains: ['api.craft.do', 'www.notion.so', 'images.unsplash.com'， 'xiaowu2.notion.site'，'s3.us-west-2.amazonaws.com']
+    domains: ['api.craft.do', 'www.notion.so', 'images.unsplash.com', 'xiaowu2.notion.site', 's3.us-west-2.amazonaws.com']
   },
   async headers() {
     return [
