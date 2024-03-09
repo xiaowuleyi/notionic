c7d76882296b870ffbce5a323b611a9e778372c3
小吴乐意 <102008938+xiaowuleyi@users.noreply.github.com>
2024-03-09 16:54:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'xiaowuleyi@gmail.com',
   link: 'https://www.xiaowuleyi.com',
   newsletter: 'Notionic Weekly',
-  description: '未来的种子，深埋在过去。',
+  description: 'The seeds of the future are buried deep in the past',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
